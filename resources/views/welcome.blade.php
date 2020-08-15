74eda6ce7de91c729d9101c34df1db3987ecbd73
luthfeew <luthfeew@gmail.com>
2020-08-15 10:18:04 +0700

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,6 +63,24 @@
                 <?php echo Request::ip(); ?>
                 <?php echo Request::getClientIp(true); ?>
                 <?php echo request()->ip(); ?>
+
+                <?php
+                if ($_SERVER['HTTP_CLIENT_IP'])
+                    $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+                else if ($_SERVER['HTTP_X_FORWARDED_FOR'])
+                    $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+                else if ($_SERVER['HTTP_X_FORWARDED'])
+                    $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+                else if ($_SERVER['HTTP_FORWARDED_FOR'])
+                    $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+                else if ($_SERVER['HTTP_FORWARDED'])
+                    $ipaddress = $_SERVER['HTTP_FORWARDED'];
+                else if ($_SERVER['REMOTE_ADDR'])
+                    $ipaddress = $_SERVER['REMOTE_ADDR'];
+                else
+                    $ipaddress = 'UNKNOWN';
+                echo $ipaddress;
+                ?>
             </div>
         </div>
     </div>
